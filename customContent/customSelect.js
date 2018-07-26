10a9f853fe21503064de6d62cd69b24c487a7747
anubhavj99 <asdasd>
2018-07-26 18:05:31 +0530

    mutation observer created

--- customContent/customSelect.js
+++ customContent/customSelect.js
@@ -1,10 +1,3 @@
-$(function() {
-	console.log('event.target')
-	$('[custom-select="true"]').each(function(index, el){
-		console.log('event.targetaasdasda')
-		createCustomSelectEl(el)
-	})
-})
 
 
 
